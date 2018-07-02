6af1da7191af36c17afec44c6510f58797aaf381
Pavel Suvorov <psuvorov@riverlogic.com>
2018-07-02 10:05:27 +0400

    Implement user registration and user login on server side.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://crm_admin:demodemo1@ds123181.mlab.com:23181/light_crm'
+    mongoURI: 'mongodb://crm_admin:demodemo1@ds123181.mlab.com:23181/light_crm',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,59 @@
-module.exports.login = function(req, res) {
-	res.status(200).json({
-		login: 'from controller'
-	})
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/Users')
+const keys = require('../config/keys')
+
+
+module.exports.login = async function(req, res) {
+	const candidate = await User.findOne({email: req.body.email})
+
+	if (candidate) {
+		const isPasswordMatch = bcrypt.compareSync(req.body.password, candidate.password)
+		if (isPasswordMatch) {
+			const token = jwt.sign({
+				email: candidate.email,
+				userId: candidate._id
+			}, keys.jwt, {expiresIn: 60*60})
+
+			res.status(200).json({
+				token: `Bearer ${token}`
+			})
+		} else {
+			res.status(401).json({
+				 message: 'Login or password is wrong.'
+			})
+		}
+	} else {
+		res.status(404).json({
+			message: 'User with such email is not found.'
+		})
+	}
 }
   
   
-module.exports.register = function(req, res) {
-	res.status(200).json({
-		register: 'from controller'
-	})
+module.exports.register = async function(req, res) {
+	const candidate = await User.findOne({email: req.body.email})
+
+	if (candidate) {
+		res.status(409).json({
+			message: 'This email is under use. Try another one.'
+		})
+	} else {
+		const salt = bcrypt.genSaltSync(10)
+		const password = req.body.password
+
+		const user = new User({
+			email: req.body.email,
+			password: bcrypt.hashSync(password, salt)
+		})
+
+		try {
+			await user.save()
+			res.status(201).json(user)
+		} catch (error) {
+			res.status(500).json({
+				message: 'Some error occurs while user registration.'
+			})
+		}
+	}
 }
