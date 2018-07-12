34d53e5320a6f65496a502b75dd4afb9e80b04e5
Pavel Suvorov <psuvorov@riverlogic.com>
2018-07-12 17:41:50 +0400

    Add main menu skeleton and overview page.

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-site-layout',
-  templateUrl: './site-layout.component.html',
-  styleUrls: ['./site-layout.component.css']
+	selector: 'app-site-layout',
+	templateUrl: './site-layout.component.html',
+	styleUrls: ['./site-layout.component.css']
 })
 export class SiteLayoutComponent implements OnInit {
 
-  constructor() { }
+	links = [
+		{url: '/overview', name: 'Overview'},
+		{url: '/analytics', name: 'Analytics'},
+		{url: '/history', name: 'History'},
+		{url: '/order', name: 'Add an order'},
+		{url: '/categories', name: 'Assortment'}
+	]
 
-  ngOnInit() {
-  }
+	constructor(private auth: AuthService, private router: Router) { }
+
+	ngOnInit() {
+		
+	}
+
+	logout(event: Event) {
+		event.preventDefault();
+		this.auth.logout();
+		this.router.navigate(['/login']);
+
+
+	}
 
 }
