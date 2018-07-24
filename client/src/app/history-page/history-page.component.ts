f80acc511bc4158fa52080408c95252193ff0f90
Pavel Suvorov <psuvorov@riverlogic.com>
2018-07-24 21:24:30 +0400

    Implement partyally order history: add forgotten files.

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrdersService} from "../shared/services/orders.service";
+import {Subscription} from "rxjs";
+import {Order} from "../shared/interfaces";
+
+const STEP = 2;
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tooltip') tooltipRef: ElementRef;
+  tooltip: MaterialInstance;
+  isFilterVisible = false;
+  oSub: Subscription;
+  orders: Order[] = [];
+
+  offset = 0;
+  limit = STEP;
+
+  loading = false;
+  reloading = false;
+  noMoreOrders = false;
+
+
+  constructor(private ordersService: OrdersService) { }
 
   ngOnInit() {
+    this.fetch();
+  }
+
+  private fetch() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    };
+    this.oSub = this.ordersService.fetch(params).subscribe(orders => {
+      this.orders = this.orders.concat(orders);
+      this.noMoreOrders = orders.length < STEP;
+      this.loading = false;
+      this.reloading = false;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef);
+  }
+
+  ngOnDestroy(): void {
+    this.tooltip.destroy();
+    this.oSub.unsubscribe();
+  }
+
+  loadMore() {
+    this.offset += STEP;
+    this.loading = true;
+    this.fetch();
   }
 
 }
